2fdfc3d5a5f1507f1d397ff040c2b84c653ed2cd
taka-ipa <taka.ipa1110@gmail.com>
2025-01-03 21:47:19 +0900

    recipe-show  added

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -87,7 +87,10 @@ class RecipeController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $recipe = Recipe::find($id);
+        dd($recipe);
+        $recipe->increment('views');
+        return view('recipes.show', compact('recipe'));
     }
 
     /**
